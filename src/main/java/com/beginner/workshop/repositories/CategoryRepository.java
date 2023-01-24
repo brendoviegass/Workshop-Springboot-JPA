c2f29b07cb62e6d977b9f63d7c5c7126419ffd9c
Brendo Viegas <brendoviess@outlook.com>
2023-01-24 20:39:01 -0200

    Category

---
+++ src/main/java/com/beginner/workshop/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.beginner.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.beginner.workshop.models.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
